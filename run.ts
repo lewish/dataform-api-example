74fc51755f336c514e5a1481c7f869a6507fbb6a
Lewis Hemens <lewishemens@google.com>
2022-05-13 14:42:53 +0100

    Add local CLI package with types on the latest version

--- run.ts
+++ run.ts
@@ -1,5 +1,4 @@
-import * as dfapi from "@dataform/api";
-import * as dbadapters from "@dataform/api/dbadapters";
+import {compile, build, run, dbadapters} from "@dataform/cli";
 import { join } from "path";
 import { cwd } from "process";
 
@@ -10,16 +9,16 @@ async function main() {
     var runConfig = { tags: ["tagtorun"] };
     var billingProjectId = "lewishemens-exp";
 
-    var compiledGraph = await dfapi.compile({ projectDir: join(cwd(), projectPath), projectConfigOverride });
+    var compiledGraph = await compile({ projectDir: join(cwd(), projectPath), projectConfigOverride });
     console.log(JSON.stringify(compiledGraph, null, 2));
     var dbadapter = await dbadapters.create({
         projectId: billingProjectId,
         location: "US",
         credentials: null // Application default credentials.
     }, "bigquery");
-    var executionGraph = await dfapi.build(compiledGraph, runConfig, dbadapter);
+    var executionGraph = await build(compiledGraph, runConfig, dbadapter);
     console.log(JSON.stringify(executionGraph, null, 2));
-    var executedGraph = await dfapi.run(executionGraph, dbadapter).result();
+    var executedGraph = await run(dbadapter, executionGraph).result();
     console.log(JSON.stringify(executedGraph, null, 2));
 }
 
